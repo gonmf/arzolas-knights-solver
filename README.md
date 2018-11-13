d160d28a0d33b1b798d72570557a08ecf6108f5e
gonmf <gonmf@sapo.pt>
2018-11-13 01:37:53 +0000

    Fix and improve README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # arzolas-knights-solver
 Simple solver for Arzola's puzzle game KNIGHTS https://store.steampowered.com/app/476240/KNIGHTS/
+
+As an example the solution for board C3 of Puzzle Pack Queen is given.
+
+Edit the knights.rb file where it says `game_string = `.
+
+For `game_string = '4;5;rxxbb..rx..xx..xx..x;b..rryyb.yy......yy.'` a possible output is:
+
+```
+Found at try #35070 (size 51, new record)
+D4-C2, A4-B2, C3-A4, B3-D4, D5-C3, D4-B3, C2-D4, B4-D5, D4-C2, B3-D4, C2-B4, A5-B3, C4-A5, B2-C4, A4-B2, C3-A4, B4-C2, D5-C3, C2-B4, B4-D5, D4-C2, C2-B4, B3-D4, D4-C2, A5-B3, C4-A5, B2-C4, B3-D4, C4-B2, A5-B3, B2-C4, C4-A5, A4-B2, C3-A4, B2-C4, A4-B2, D5-C3, B4-D5, C3-A4, D5-C3, C2-B4, D4-C2, B4-D5, C2-B4, B3-D4, B4-C2, A5-B3, D5-B4, C4-A5, B2-C4, B4-D5
+```
+
+## Puzzle string format
+
+A string like `4;5;rxxbb..rx..xx..xx..x;b..rryyb.yy......yy.` has four components separated by a semicolons:
+
+- width of the board
+- height of the board
+- list from left to right, top to bottom, of the board (free, blocked or goal color)
+- list from left to right, top to bottom, of the board pieces (knights)
+
+## Solution format
+
+The solution is given for a board using chess coordinates, for instance for 4x5:
+
+```
+  A  B  C  D
+5 .  .  .  . 5
+4 .  .  .  . 4
+3 .  .  .  . 3
+2 .  .  .  . 2
+1 .  .  .  . 1
+  A  B  C  D
+```
--- knights.rb
+++ knights.rb
@@ -2,7 +2,20 @@ def read_game_string(str)
   width, height, board, pieces = str.downcase.split(';')
   raise 'Illegal board' if pieces.nil?
 
-  width = width.to_i
+  width = width.to_i + 4
+  height = height.to_i + 4
+
+  lg_board = (0..(width * 2 - 1)).map { 'x' }.join
+  board.chars.each_slice(width - 4) do |chrs|
+    lg_board += "xx#{chrs.join}xx"
+  end
+  lg_board += (0..(width * 2 - 1)).map { 'x' }.join
+
+  lg_pieces = (0..(width * 2 - 1)).map { '.' }.join
+  pieces.chars.each_slice(width - 4) do |chrs|
+    lg_pieces += "..#{chrs.join}.."
+  end
+  lg_pieces += (0..(width * 2 - 1)).map { '.' }.join
 
   knight_moves = [
     -2 - width,
@@ -11,15 +24,15 @@ def read_game_string(str)
      2 - width,
      2 + width,
      1 + width + width,
-     1 + width + width,
+    -1 + width + width,
     -2 + width
   ]
 
   {
     width: width,
-    height: height.to_i,
-    board: board,
-    pieces: pieces,
+    height: height,
+    board: lg_board,
+    pieces: lg_pieces,
     knight_moves: knight_moves.freeze
   }
 end
@@ -44,7 +57,6 @@ def valid_plays(game)
       end
 
       candidates = candidates.select do |pos|
-        pos >= 0 && pos < game[:width] * game[:height] &&
         game[:board][pos] != 'x' && game[:pieces][pos] == '.'
       end
 
@@ -56,39 +68,47 @@ def valid_plays(game)
 end
 
 def game_over?(game)
-  ((0..(game[:width] * game[:height])).find do |num|
+  ((0..(game[:width] * game[:height] - 1)).find do |num|
     game[:board][num] == 'r' && game[:pieces][num] != 'r' ||
     game[:board][num] == 'b' && game[:pieces][num] != 'b'
   end).nil?
 end
 
-def random_search(game, depth, depth_limit)
+def random_search(game, depth, depth_limit, past_pieces)
   depth += 1
   return [nil] if depth >= depth_limit
 
-  while true do
-    possible_plays = valid_plays(game)
-    return [nil] if possible_plays.empty? # dead end
+  possible_plays = valid_plays(game)
 
-    from, to = possible_plays.sample
+  possible_plays = possible_plays.select do |pair|
+    from, to = pair
 
-    color = game[:pieces][from]
-    game[:pieces][from] = '.'
-    game[:pieces][to] = color
+    pieces = game[:pieces].dup
+    pieces[to] = pieces[from]
+    pieces[from] = '.'
 
-    if game_over?(game)
-      return ['END']
-    end
-
-    return ["#{from},#{to}"] + random_search(game, depth, depth_limit)
+    !past_pieces.include?(pieces.hash)
   end
+
+  return [nil] if possible_plays.empty? # dead end
+
+  from, to = possible_plays.sample
+
+  game[:pieces][to] = game[:pieces][from]
+  game[:pieces][from] = '.'
+
+  return [nil] if past_pieces.include?(game[:pieces].hash)
+
+  return ["#{from},#{to}", 'END'] if game_over?(game)
+
+  ["#{from},#{to}"] + random_search(game, depth, depth_limit, past_pieces + [game[:pieces].hash])
 end
 
 def pretty_pos(game, pos)
   pos = pos.to_i
 
-  left = pos % game[:width]
-  top = pos / game[:width]
+  left = (pos % game[:width]) - 2
+  top = (pos / game[:width]) + 2
 
   "#{('A'.ord + left).chr}#{game[:height] - top}"
 end
@@ -108,16 +128,20 @@ end
 def start_search(game)
   tries = 0
   best_result = nil
-  depth_limit = 20
+  depth_limit = 80
 
   while true do
     tries += 1
+    print "\r#{tries}" if (tries % 1024) == 0
 
-    result = random_search(game_dup(game), 0, depth_limit)
+    new_game = game_dup(game)
+    result = random_search(new_game, 0, depth_limit, [new_game[:pieces].hash])
 
     if result[result.count - 1] == 'END'
+      result = result[0..(result.count - 2)]
       if best_result.nil? || best_result.count > result.count
-        puts "Found at try ##{tries} (size #{result.count - 1}, new record)"
+        puts
+        puts "Found at try ##{tries} (size #{result.count}, new record)"
         best_result = result.dup
         depth_limit = result.count
         puts pretty_play_sequence(game, best_result)
@@ -126,8 +150,13 @@ def start_search(game)
   end
 end
 
+# Puzzle pack Queen board C3:
 game_string = '4;5;rxxbb..rx..xx..xx..x;b..rryyb.yy......yy.'
-# Solution:
-# D4-B2, B4-C2, D5-B4, B3-D4, A5-B3, B3-D5, C4-A5, A5-B3, A4-C4, C4-A5, B2-A4, D4-B2
 
-puts start_search(read_game_string(game_string))
+# Output:
+# Found at try #35070 (size 51, new record)
+# D4-C2, A4-B2, C3-A4, B3-D4, D5-C3, D4-B3, C2-D4, B4-D5, D4-C2, B3-D4, C2-B4, A5-B3, C4-A5, B2-C4, A4-B2, C3-A4, B4-C2, D5-C3, C2-B4, B4-D5, D4-C2, C2-B4, B3-D4, D4-C2, A5-B3, C4-A5, B2-C4, B3-D4, C4-B2, A5-B3, B2-C4, C4-A5, A4-B2, C3-A4, B2-C4, A4-B2, D5-C3, B4-D5, C3-A4, D5-C3, C2-B4, D4-C2, B4-D5, C2-B4, B3-D4, B4-C2, A5-B3, D5-B4, C4-A5, B2-C4, B4-D5
+
+game = read_game_string(game_string)
+
+start_search(game)
